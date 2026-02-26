ba288c6424417c651d370a142ce7616ece329026
yaphet2015 <yaphetyan@foxmail.com>
2026-02-26 14:12:59 +0800

    chore: rm useless content

--- dist-lib/whistle.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    namespace NodeJS {
-        interface Process {
-            handleUncauthtWhistleErrorMessage?: (stack: string, err: Error) => void;
-        }
-    }
-}
-export {};
-//# sourceMappingURL=whistle.d.ts.map
