73127784c6b7d1d9fb8aa30b94ec797bf5b942b8
yaphet <>
2026-02-26 14:12:59 +0800

    chore: rm useless content

--- dist-lib/fork.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Fork the Whistle utility process
- * Starts the Whistle proxy server in a separate utility process
- *
- * @param restart - Whether to restart an existing instance (closes current first)
- */
-declare const forkWhistle: (restart?: boolean) => void;
-export default forkWhistle;
-//# sourceMappingURL=fork.d.ts.map
--- dist-lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-import './patch';
-//# sourceMappingURL=index.d.ts.map
